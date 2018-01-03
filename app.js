9a1846aa78beb4cd17e5324845848c23cdfc0139
Scott A. Lawrence <slawrence@gmail.com>
2018-01-02 22:47:22 -0500

    Notes app implementation in progress

---
+++ app.js
@@ -0,0 +1,37 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ',command);
+console.log('Yargs',argv);
+
+if(command === 'add'){
+  var note = notes.addNote(argv.title, argv.body);
+  if (note){
+    console.log('Note created');
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Title: ${note.body}`);
+  } else {
+    console.log('Note title already taken')
+  }
+} else if (command === 'list'){
+  var list = notes.getAll();
+  if (list){
+    console.log(list);
+  } else {
+    console.log('No notes written yet.');
+  }
+} else if (command === 'read'){
+  notes.getNote(argv.title);
+} else if (command === 'remove'){
+  notes.removeNote(argv.title);
+} else{
+  console.log('Command not recognized');
+}
