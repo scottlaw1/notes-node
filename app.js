65db608240ac26356627c731855869625f2d45f2
Scott A. Lawrence <slawrence@gmail.com>
2018-01-03 21:57:59 -0500

    Implemented note removal capability
    Implemented note removal capability and notification of success (or not
    found).

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ if(command === 'add'){
 } else if (command === 'read'){
   notes.getNote(argv.title);
 } else if (command === 'remove'){
-  notes.removeNote(argv.title);
+  var removed = notes.removeNote(argv.title);
+  var message = removed ? 'Note was removed' : 'Note not found';
+  console.log(message);
 } else{
   console.log('Command not recognized');
 }
